0f1086795fb5b6907891506d43c0ed4d58e288dd
Joe Panetta <jlpanetta1681@icloud.com>
2021-11-25 19:16:18 -0500

    Initial commit weather app ios

---
+++ ViewController.swift
@@ -0,0 +1,75 @@
+//
+//  ViewController.swift
+//  MyWeather2
+// main
+//  Created by joe panetta on 11/25/21.
+//
+
+import CoreLocation
+import UIKit
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, CLLocationManagerDelegate {
+    
+    @IBOutlet var table: UITableView!
+    var models = [Weather]()
+
+    let locationManager = CLLocationManager()
+
+    var currentLocation: CLLocation?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // register two cells
+        table.register(HourlyTableViewCell.nib(), forCellReuseIdentifier: HourlyTableViewCell.identifier)
+        table.register(WeatherTableViewCell.nib(), forCellReuseIdentifier: WeatherTableViewCell.identifier)
+
+        table.delegate = self
+        table.dataSource = self
+        
+    
+}
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        setupLocation()
+    }
+
+    // Location
+
+    func setupLocation() {
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if !locations.isEmpty, currentLocation == nil {
+            currentLocation = locations.first
+            locationManager.stopUpdatingLocation()
+            requestWeatherForLocation()
+        }
+    }
+
+    func requestWeatherForLocation() {
+        guard let currentLocation = currentLocation else {
+            return
+        }
+        let long = currentLocation.coordinate.longitude
+        let lat = currentLocation.coordinate.latitude
+    
+        print("\(long) | \(lat)")
+    }
+
+    // Table
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return models.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+struct Weather {
+}
